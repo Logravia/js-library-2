de6850b1b1138cd2c9d865a2d5f71036642d5217
Jānis Alunāns <j.alunans@gmail.com>
2022-12-06 01:01:47 +0200

    Add delete, read-unread, edit functionality

--- script.js
+++ script.js
@@ -1,60 +1,122 @@
-let myLibrary = [new Book("The Hobbit", "J.R.R. Tolkien", 1937)];
+let myLibrary = [];
 
-let table = document.querySelector("#book-table");
+const table = document.querySelector("#book-table");
+const inputs = document.querySelectorAll("input");
+const saveBtn = document.querySelector("#save");
 
-function Book(title, author, pubDate, rating=0, read=false) {
+function Book(title="", author="", published=1990, rating=0, read=false) {
   this.title = title;
   this.author = author;
-  this.pubDate = pubDate;
+  this.published = published;
   this.rating = rating;
   this.read = read;
 }
 
-Book.prototype = {
-  toggleRead: ()=> this.read = !this.read
+Book.prototype.toggleRead = function() {
+  this.read = !this.read;
 }
 
-const addBookToLibrary = data => {
-  myLibrary.push(new Book(data.title, data.author, data.pubDate,
-                          data.rating, data.read));
+const addBook = function (book) {
+  myLibrary.push(book);
 }
 
-const newRow = (book, id=0) => {
+const newRow = function (book, id=0) {
   let row = document.createElement("tr");
 
   for (prop in book) { // fill cells with prop vals
-    let cell = document.createElement("td");
-    cell.textContent = book[prop];
-    row.appendChild(cell);
+
+    if (Object.prototype.hasOwnProperty.call(book, prop)) {
+      let cell = document.createElement("td");
+      cell.textContent = book[prop];
+      row.appendChild(cell);
+    }
   }
     row.appendChild(newDelBtn(id));
+    row.appendChild(newReadBtn(id));
+    row.appendChild(newEditBtn(id));
     table.appendChild(row);
 }
 
-const displayLibrary = () => {
+const displayLibrary = function() {
   for (let i = 0; i < myLibrary.length; i++) {
     newRow(myLibrary[i], i);
   }
 }
 
-const refreshDisplay = () => {
+const refreshDisplay = function() {
   table.replaceChildren();
   displayLibrary();
 }
 
-const removeBook = id => {
+const clearForm = function () {
+  inputs.forEach((element)=>{
+    element.value="";
+  });
+  elements[4].checked=false;
+};
+
+const fillForm = function (book) {
+  inputs.forEach((element)=>{
+    element.value=book[element.name];
+  });
+  inputs[4].checked=book.read;
+}
+
+const removeBook = function(id) {
   myLibrary.splice(id,1);
 }
 
-const newDelBtn = id => {
+const newReadBtn = function(id) {
   btn = document.createElement("button");
-  btn.id = id;
-  btn.textContent = "DELETE"
+  btn.value = id;
+  btn.textContent = myLibrary[id].read ? "Read" : "Unread";
   btn.addEventListener("click",e=>{
-    removeBook(e.currentTarget.id);
+    index = Number(e.currentTarget.value);
+    myLibrary[index].toggleRead();
     refreshDisplay();
   });
   return btn;
 }
+
+const newDelBtn = function(id) {
+  btn = document.createElement("button");
+  btn.value = id;
+  btn.textContent = "Delete"
+  btn.addEventListener("click",e=>{
+    removeBook(e.currentTarget.value);
+    refreshDisplay();
+  });
+  return btn;
+}
+
+const newEditBtn = function(id) {
+  btn = document.createElement("button");
+  btn.value = id;
+  btn.textContent = "Edit"
+
+  btn.addEventListener("click",e=>{
+    index = e.currentTarget.value;
+    bookToEdit = myLibrary[index];
+    fillForm(bookToEdit);
+    removeBook(index);
+    refreshDisplay();
+  });
+  return btn;
+}
+
+const gatherFormData = function () {
+  let book = new Book;
+  inputs.forEach((element)=>{
+    book[element.name] = element.value;
+  });
+  book.read = inputs[4].checked;
+  return book;
+}
+
+saveBtn.addEventListener("click", (e)=>{
+  addBook(gatherFormData());
+  refreshDisplay();
+  clearForm();
+})
 
 displayLibrary();
